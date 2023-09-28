6758df7ae40c32f2a268055bfc482188355cd06a
Lunafanci <142280093+Lunafanci@users.noreply.github.com>
2023-09-28 22:11:38 +0700

    lananh

--- phone.php
+++ phone.php
@@ -27,7 +27,7 @@ $data=mysqli_query($con,$sql);
             <td><img class="card-img-top" src="<?php echo $row['img'] ?>" width="200x" height="200x" name="img" ></td>
             <tr><td ><?php echo $row['name'] ?></td></tr><input type="hidden" name="name" value="<?php echo $row['name']?>">
             <tr><td><?php echo $row['price'] ?></td></tr><input type="hidden" name="price" value="<?php echo $row['price']?>">
-            <tr><td><?php echo $row['id'] ?></td></tr><input type="hidden" name="id" value="<?php echo $row['id']?>">
+            <tr><td> <input type="hidden" name="id" value="<?php echo $row['id']?>"></td></tr>
             <td> 
                 <a  href="./addcart.php?id=<?php echo $row['id'] ?>&&name=<?php echo $row['name']?>&&price=<?php echo $row['price']?>">Thêm giỏ hàng</a>&nbsp;&nbsp;
                 <input class="btn btn-primary" type="submit" value="Mua ngay" name="buy">   
